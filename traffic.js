bde04699694ab083e3aa76622deb87005c330289
PANNAKARA SAIKIRAN <118048369+pannakarasaikiran@users.noreply.github.com>
2023-02-27 23:15:32 +0530

    Add files via upload

---
+++ traffic.js
@@ -0,0 +1,14 @@
+function turnOnRed() {
+    document.getElementById("stopLight").style.backgroundColor = "red";
+    document.getElementById("stopButton").style.backgroundColor = "red";
+}
+
+function turnOnYellow() {
+    document.getElementById("readyLight").style.backgroundColor = "yellow";
+    document.getElementById("readyButton").style.backgroundColor = "yellow";
+}
+
+function turnOnGreen() {
+    document.getElementById("goLight").style.backgroundColor = "green";
+    document.getElementById("goButton").style.backgroundColor = "green";
+}
